884a79e44ccba674e28b9fdcb0199644e0da4841
Pattaraporn Tonsomboon <pattaraporn.tos@student.mahidol.ac.th>
2025-03-24 15:44:54 +0700

    Add appBar file by 093

---
+++ appBar.dart
@@ -0,0 +1,15 @@
+appBar: AppBar(
+        toolbarHeight: 100,
+        title: Padding(
+          padding: EdgeInsets.only(top: 35, left: 15),
+          child: Text(
+            'Workout',
+            style: TextStyle(
+              fontFamily: "KoPub", 
+              fontWeight: FontWeight.bold,
+              fontSize: 35
+            ),
+          ),
+        ),
+        backgroundColor: Color(0xFFE5D7C4),
+      ),
