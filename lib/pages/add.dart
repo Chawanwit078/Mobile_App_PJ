09e6c083af7f522f256e432cd7159623913a1ca0
lookchub <Ceekay662005@gmail.com>
2025-04-07 20:40:33 +0700

    commit testtest

---
+++ lib/pages/add.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Color(0xFFE5D7C4),
+      appBar: AppBar(title: Text('Add')),
+    );
+  }
+}
